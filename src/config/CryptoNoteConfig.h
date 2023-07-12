fdfadf10095495336457ec8648153d4a06faeaf0
traaitt | TRRXITTE inc <traaitt@icloud.com>
2023-07-12 23:01:36 +0200

    implementation for obfuscation limits
    The optimized implementation for anonymization services.

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -89,28 +89,29 @@ namespace CryptoNote
         const uint64_t MINIMUM_FEE = UINT64_C(10);
 
         /* This section defines our minimum and maximum mixin counts required for transactions */
-        const uint64_t MINIMUM_MIXIN_V1                              = 3;
+        const uint64_t MINIMUM_MIXIN_V1                              = 0;
 
         const uint64_t MAXIMUM_MIXIN_V1                              = 10;
 
-        const uint64_t MINIMUM_MIXIN_V2                              = 10;
+        const uint64_t MINIMUM_MIXIN_V2                              = 0;
 
-        const uint64_t MAXIMUM_MIXIN_V2                              = 10;
+        const uint64_t MAXIMUM_MIXIN_V2                              = 15;
 
-        const uint64_t MINIMUM_MIXIN_V3                              = 15;
+        const uint64_t MINIMUM_MIXIN_V3                              = 0;
 
         const uint64_t MAXIMUM_MIXIN_V3                              = 15;
 
         /* The heights to activate the mixin limits at */
-        const uint32_t MIXIN_LIMITS_V1_HEIGHT = 17000000;
+        const uint32_t MIXIN_LIMITS_V1_HEIGHT = 1000000;
 
-        const uint32_t MIXIN_LIMITS_V2_HEIGHT = 43000000;
+        const uint32_t MIXIN_LIMITS_V2_HEIGHT = 2400000;
 
-        const uint32_t MIXIN_LIMITS_V3_HEIGHT = 68000000;
+        const uint32_t MIXIN_LIMITS_V3_HEIGHT = 4500000;
 
         /* The mixin to use by default with cmdedition and service application */
         /* DEFAULT_MIXIN_V0 is the mixin used before MIXIN_LIMITS_V1_HEIGHT is started */
-        const uint64_t DEFAULT_MIXIN_V0 = 3;
+        /* static mixing 18 for static implementation requirements obfuscation at height 4500000 */
+        const uint64_t DEFAULT_MIXIN_V0 = 18;
 
         const uint64_t DEFAULT_MIXIN_V1 = MAXIMUM_MIXIN_V1;
 
