6c3339092a4e16a435b9375b67ebd40dd2b0b7d1
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2023-09-13 22:33:01 +0200

    MINIMUM_FEE = UINT64_C(100);

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -86,7 +86,7 @@ namespace CryptoNote
 
         const size_t CRYPTONOTE_DISPLAY_DECIMAL_POINT = 2;
 
-        const uint64_t MINIMUM_FEE = UINT64_C(10);
+        const uint64_t MINIMUM_FEE = UINT64_C(100);
 
         /* This section defines our minimum and maximum mixin counts required for transactions */
         const uint64_t MINIMUM_MIXIN_V1                              = 0;
